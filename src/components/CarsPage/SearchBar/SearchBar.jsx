53b8fb5ffd8d6fa69d3acf2ed06b79c76653a312
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-09 13:05:23 +0100

    update

--- src/components/CarsPage/SearchBar/SearchBar.jsx
+++ src/components/CarsPage/SearchBar/SearchBar.jsx
@@ -1,19 +1,58 @@
 import { marks } from "lib/marks";
-import { LabelSelect, SearchContainer, SelectContainer } from "./SearchBar.styled";
+import Select from "react-select";
+import makeAnimated from "react-select/animated";
+import {
+  LabelSelect,
+  SearchBtn,
+  SearchContainer,
+  SelectContainer,
+} from "./SearchBar.styled";
 
 const SearchBar = () => {
   return (
     <SearchContainer>
       <SelectContainer>
-      <LabelSelect htmlFor="marks">Car brand</LabelSelect>
-      <select name="marks" id="marks">
-        <option value="">Enter the text</option>
-        {marks.map((mark) => (
-          <option value={mark.toLowerCase()} key={mark}>
-            {mark}
-          </option>
-        ))}
-      </select></SelectContainer>
+        <LabelSelect htmlFor="marks">Car brand</LabelSelect>
+        <Select
+          options={marks}
+          makeAnimated={makeAnimated}
+          placeholder={"Enter the text"}
+          maxMenuHeight={272}
+          isClearable={true}
+          styles={{
+            control: (baseStyles) => ({
+              ...baseStyles,
+              border: "none",
+              width: "224px",
+              height: "48px",
+              background: "#f7f7fb",
+              borderRadius: "14px",
+              fontSize: "18px",
+              fontWeight: "500",
+              color: "black",
+              fontFamily: "Manrope",
+              cursor: "pointer",
+            }),
+            option: (baseStyles, state) => ({
+              ...baseStyles,
+              border: "none",
+              fontSize: "16px",
+              fontWeight: "500",
+              fontFamily: "Manrope",
+              lineHeight: "125%",
+              color: state.isFocused
+                ? "rgba(18, 20, 23, 0.938)"
+                : "rgba(18, 20, 23, 0.2)",
+            }),
+            valueContainer: (baseStyles) => ({
+              ...baseStyles,
+              borderRadius: "14px",
+              boxShadow: "0 4px 36px 0 rgba(0, 0, 0, 0.02)",
+            })
+          }}
+        />
+      </SelectContainer>
+      <SearchBtn type="button">Search</SearchBtn>
     </SearchContainer>
   );
 };
--- src/components/CarsPage/SearchBar/SearchBar.styled.jsx
+++ src/components/CarsPage/SearchBar/SearchBar.styled.jsx
@@ -3,6 +3,9 @@ import styled from "styled-components";
 export const SearchContainer = styled.div`
   margin: 24px 0;
   padding: 0 40px;
+  display: flex;
+  align-items: flex-end;
+  column-gap: 24px;
 `;
 
 export const SelectContainer = styled.div`
@@ -19,6 +22,21 @@ export const LabelSelect = styled.label`
   color: #8a8a89;
 `;
 
-
-
-
+export const SearchBtn = styled.button`
+  outline: none;
+  border: none;
+  background: #3470ff;
+  border-radius: 12px;
+  padding: 14px 44px;
+  width: 136px;
+  height: 48px;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 143%;
+  color: #fff;
+  transition: all 400ms ease;
+  &:hover,
+  &:focus {
+    background: #0046e9;
+  }
+`;

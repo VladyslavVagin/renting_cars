534fc99b1ac0bd71ac4168e0cf6e632193072478
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-10 00:03:58 +0100

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,33 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { carsReducer } from './carsSlice';
 
+const persistConfig = {
+  key: 'cars',
+  storage,
+};
+
+
 export const store = configureStore({
-    reducer: {
-      cars: carsReducer,
-    }
-  });
+  reducer: {
+    cars:  persistReducer(persistConfig, carsReducer),
+  },
+  middleware: getDefaultMiddleware => 
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  })
+});
+
+export const persistor = persistStore(store);

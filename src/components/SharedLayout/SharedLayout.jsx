c210358c886a68cf509f7704e5fde1f1ff157be0
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-10 21:09:03 +0100

    update

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,16 +1,12 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
 import Header from "components/Header/Header";
 import Loader from "components/Loader/Loader";
-import HelmetCEO from "components/HelmetCEO/Helmet";
 import { LoaderContainer } from "./SharedLayout.styled";
 
 const SharedLayout = () => {
   return (
     <>
-      <HelmetProvider>
-        <HelmetCEO/>
         <Header />
         <main>
           <Suspense
@@ -25,7 +21,6 @@ const SharedLayout = () => {
             <Outlet />
           </Suspense>
         </main>
-      </HelmetProvider>
     </>
   );
 };
